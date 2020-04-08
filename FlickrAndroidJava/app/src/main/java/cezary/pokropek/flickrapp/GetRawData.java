677dd7aa15d60dec1d4a42769cdd646bc50bbd3a
Cezary-Pokropek <cezary.pokropek@gmail.com>
2020-04-08 20:00:28 +0200

    Prototype backend code made to the app including async tasks, dealing with json format and URL sync

---
+++ FlickrAndroidJava/app/src/main/java/cezary/pokropek/flickrapp/GetRawData.java
@@ -0,0 +1,116 @@
+package cezary.pokropek.flickrapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+// enum to hold a list of download statutes
+enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALIZED, FAILED_OR_EMPTY, OK}
+
+class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG = "GetRawData";
+
+    private DownloadStatus mDownloadStatus;
+    private final OnDownloadComplete mCallback;
+
+    interface OnDownloadComplete {
+        void onDownloadComplete(String data, DownloadStatus status);
+    }
+
+    public GetRawData(OnDownloadComplete callback) {
+        this.mDownloadStatus = DownloadStatus.IDLE;
+        mCallback = callback;
+    }
+
+    void runInSameThread(String s) {
+        Log.d(TAG, "runInSameThread: starts ");
+
+//        onPostExecute(doInBackground(s));
+        if (mCallback != null) {
+//            String result = doInBackground(s);
+//            mCallback.onDownloadComplete(result,mDownloadStatus);
+            mCallback.onDownloadComplete(doInBackground(s),mDownloadStatus);
+        }
+
+        Log.d(TAG, "runInSameThread: ends ");
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        Log.d(TAG, "onPostExecute: parameter = " + s);
+        // calling main activities on downloadComplete and provide it with the data
+        // and the status result
+        if (mCallback != null) {
+            mCallback.onDownloadComplete(s, mDownloadStatus);
+        }
+        Log.d(TAG, "onPostExecute: ends");
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        // check if valid URL
+        if (strings == null) {
+            mDownloadStatus = DownloadStatus.NOT_INITIALIZED;
+            return null;
+        }
+
+        try {
+            // attempt to create a URL from the string parameter
+            mDownloadStatus = DownloadStatus.PROCESSING;
+            URL url = new URL(strings[0]);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            Log.d(TAG, "doInBackground: The response code was " + response);
+
+            StringBuilder result = new StringBuilder();
+
+            // setting up buffered reader to read data on input stream and adding that to the string builder
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            // reading a line at a time
+            String line;
+            while (null != (line = reader.readLine())) {
+                result.append(line).append("\n");
+            }
+
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "doInBackground: Invalid URL " + e.getMessage() );
+        } catch (IOException e ) {
+            Log.e(TAG, "doInBackground: IO Exception reading data " + e.getMessage() );
+        } catch (SecurityException e) {
+            Log.e(TAG, "doInBackground: Security Exception. Needs permission? " + e.getMessage() );
+        } finally {
+            if (connection != null){
+                connection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch(IOException e) {
+                    Log.e(TAG, "doInBackground: Error closing stream " + e.getMessage() );
+                }
+            }
+        }
+
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+        return null;
+    }
+
+
+
+}
---
+++ FlickrAndroidJava/app/src/main/java/cezary/pokropek/flickrapp/Photo.java
@@ -0,0 +1,48 @@
+package cezary.pokropek.flickrapp;
+
+class Photo {
+
+    private String mTitle;
+    private String mAuthor;
+    private String mAuthorId;
+    private String mLink;
+    private String mTags;
+    private String mImage;
+
+    public Photo(String title, String author, String authorId, String link, String tags, String image) {
+        this.mTitle = title;
+        this.mAuthor = author;
+        this.mAuthorId = authorId;
+        this.mLink = link;
+        this.mTags = tags;
+        this.mImage = image;
+    }
+
+    String getTitle() { return mTitle; }
+
+    String getAuthor() { return mAuthor; }
+
+    String getAuthorId() { return mAuthorId; }
+
+    String getLink() { return mLink; }
+
+    String getTags() { return mTags; }
+
+    String getImage() { return mImage; }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mAuthorId='" + mAuthorId + '\'' +
+                ", mLink='" + mLink + '\'' +
+                ", mTags='" + mTags + '\'' +
+                ", mImage='" + mImage + '\'' +
+                '}';
+    }
+
+
+
+
+}
